17739bcdc2aa1fc3e920f55d9b5727a052982abe
Jakub Bujakiewicz <kubabujakiewicz@op.pl>
2026-09-11 11:25:14 +0200

    Zadanie4

--- Zadania01/src/Main.java
+++ Zadania01/src/Main.java
@@ -1,10 +1,13 @@
+import java.util.Scanner;
 import static java.lang.IO.print;
 
 void main() {
+    // Zad. 1
     print("Ania");
     print("Bartek");
     print("Kasia");
 
+   // Zad. 2
     String imie = "Jakub";
     int rokUrodzenia = 2007;
     double liczba = 0.66;
@@ -13,5 +16,20 @@ void main() {
     print(rokUrodzenia);
     print(liczba);
 
+    // Zad. 3
     print("Mam na imię " + imie + ", mam " + (2026 - rokUrodzenia) + " lat i będę pisać maturę za " + liczba + " roku.");
+
+    // Zad. 4
+    Scanner scanner = new Scanner(System.in);
+
+    double stopnie;
+
+    print("Podaj temperaturę w stopniach Celsjusza: ");
+    stopnie = scanner.nextDouble();
+
+    double fahrenheit = 1.8 * stopnie + 32.0;
+
+    print("Temperatura w stopniach Fahrenheita: " + fahrenheit);
 }
+
+    
