3a0c8dcf5c98cab9d6ca509f74fa6ee320a19f41
Jakub Bujakiewicz <kubabujakiewicz@op.pl>
2026-09-11 11:18:31 +0200

    Zadanie2

--- Zadania01/src/Main.java
+++ Zadania01/src/Main.java
@@ -4,5 +4,13 @@ void main() {
     print("Ania");
     print("Bartek");
     print("Kasia");
+
+    String imie = "Jakub";
+    int rokUrodzenia = 2007;
+    double liczba = 0.66;
+
+    print(imie);
+    print(rokUrodzenia);
+    print(liczba);
     
 }
