63849b4d7511cb9b0cb7bc0f42d4bffd4522f290
Jakub Bujakiewicz <kubabujakiewicz@op.pl>
2026-09-18 10:28:03 +0200

    Zadanie2 - Prawa Autorskie

--- Zadania02/src/Main.java
+++ Zadania02/src/Main.java
@@ -1,77 +1,87 @@
+import java.util.Scanner;
+
 void main() {
-  //1
+
+  // ZADANIE WYKONANE PRZEZ JAKUBA BUJAKIEWICZA //
+
   Scanner sc = new Scanner(System.in);
+
+  //1
   System.out.print("Podaj liczbe: ");
   int liczba = sc.nextInt();
-  if(liczba%3==0){
-    System.out.println("Twoja Liczba jest podzielna przez 3");
-  }else{
+
+  if (liczba % 3 == 0) {
+    System.out.println("Twoja liczba jest podzielna przez 3");
+  } else {
     System.out.println("Twoja liczba nie jest podzielna przez 3");
   }
 
-  //2
-  Scanner sc2 = new Scanner(System.in);
+  // ZADANIE WYKONANE PRZEZ JAKUBA BUJAKIEWICZA //
 
+  //2
   System.out.print("Podaj 1 bok trójkąta: ");
-  int bok1 = sc2.nextInt();
+  int bok1 = sc.nextInt();
 
   System.out.print("Podaj 2 bok trójkąta: ");
-  int bok2 = sc2.nextInt();
+  int bok2 = sc.nextInt();
 
   System.out.print("Podaj 3 bok trójkąta: ");
-  int bok3 = sc2.nextInt();
+  int bok3 = sc.nextInt();
 
-  if(bok1 + bok2 > bok3 && bok1 + bok3 > bok2 && bok2 + bok3 > bok1){
+  if (bok1 + bok2 > bok3 && bok1 + bok3 > bok2 && bok2 + bok3 > bok1) {
     System.out.println("Z twoich liczb można zbudować trójkąt");
-  }else{
-    System.out.println("Z twoich liczb niemożna zbudować trójkąta");
+  } else {
+    System.out.println("Z twoich liczb nie można zbudować trójkąta");
   }
+
+  // ZADANIE WYKONANE PRZEZ JAKUBA BUJAKIEWICZA //
 
   //3
-  Scanner sc3 = new Scanner(System.in);
+  System.out.print("Podaj liczbe: ");
+  int a = sc.nextInt();
 
   System.out.print("Podaj liczbe: ");
-  int a = sc3.nextInt();
+  int b = sc.nextInt();
 
-  System.out.print("Podaj liczbe: ");
-  int b = sc3.nextInt();
-
-  if(a>b){
-    System.out.println("Większe jest: "+a);
-  }else if(b>a){
-    System.out.println("Większe jest: "+b);
+  if (a > b) {
+    System.out.println("Większe jest: " + a);
+  } else if (b > a) {
+    System.out.println("Większe jest: " + b);
+  } else {
+    System.out.println("Liczby są równe");
   }
 
+  // ZADANIE WYKONANE PRZEZ JAKUBA BUJAKIEWICZA //
+
   //4
-  Scanner sc4 = new Scanner(System.in);
+  System.out.print("Podaj liczbe: ");
+  int a2 = sc.nextInt();
 
   System.out.print("Podaj liczbe: ");
-  int a2 = sc4.nextInt();
+  int b2 = sc.nextInt();
 
   System.out.print("Podaj liczbe: ");
-  int b2 = sc4.nextInt();
-
-  System.out.print("Podaj liczbe: ");
-  int c2 = sc4.nextInt();
+  int c2 = sc.nextInt();
 
   int najwieksza = a2;
 
-  if(b2>najwieksza){
+  if (b2 > najwieksza) {
     najwieksza = b2;
   }
-  if(c2>najwieksza){
+
+  if (c2 > najwieksza) {
     najwieksza = c2;
   }
 
   System.out.println("Najwieksza liczba to " + najwieksza);
 
+  // ZADANIE WYKONANE PRZEZ JAKUBA BUJAKIEWICZA //
+
   //5
-  Scanner sc5 = new Scanner(System.in);
-
   System.out.print("Podaj numer miesiaca: ");
-  int miesiac = sc5.nextInt();
+  int miesiac = sc.nextInt();
 
-  String nazwa = switch(miesiac){
+  String nazwa = switch (miesiac) {
     case 1 -> "Styczen";
     case 2 -> "Luty";
     case 3 -> "Marzec";
@@ -86,88 +96,88 @@ void main() {
     case 12 -> "Grudzien";
     default -> "Nieprawidlowy numer miesiaca";
   };
+
   System.out.println(nazwa);
 
-  //6
-  Scanner sc6 = new Scanner(System.in);
+  // ZADANIE WYKONANE PRZEZ JAKUBA BUJAKIEWICZA //
 
-  System.out.print("Podaj inie: ");
-  String imie = sc6.next();
+  //6
+  System.out.print("Podaj imie: ");
+  String imie = sc.next();
 
   String mojeImie = "Jakub";
-  if(imie.equals(mojeImie)){
+
+  if (imie.equalsIgnoreCase(mojeImie)) {
     System.out.println("Masz takie samo imie jak ja");
-  }else{
-    System.out.println("Masz inne imie niż ja");
+  } else {
+    System.out.println("Masz inne imie niz ja");
   }
 
+  // ZADANIE WYKONANE PRZEZ JAKUBA BUJAKIEWICZA //
 
+  //7
+  System.out.print("Podaj swoj wiek: ");
+  int wiek = sc.nextInt();
 
-//7
-Scanner sc7 = new Scanner(System.in);
+  boolean pelnoletni = wiek >= 18 ? true : false;
 
-System.out.print("Podaj swoj wiek: ");
-int wiek = sc7.nextInt();
+  System.out.println("Czy jestes pelnoletni? " + pelnoletni);
 
-boolean pelnoletni = wiek >= 18 ? true : false;
+  // ZADANIE WYKONANE PRZEZ JAKUBA BUJAKIEWICZA //
 
-System.out.println("Czy jestes pelnoletni? " + pelnoletni);
-
-//8
-  Scanner sc8 = new Scanner(System.in);
-
+  //8
   System.out.print("Podaj rok: ");
-  int rok = sc8.nextInt();
+  int rok = sc.nextInt();
 
-  if((rok % 4 == 0 && rok % 100 != 0) || rok % 400 == 0){
+  if ((rok % 4 == 0 && rok % 100 != 0) || rok % 400 == 0) {
     System.out.println("Podany rok jest przestepny");
-  }else{
+  } else {
     System.out.println("Podany rok nie jest przestepny");
   }
 
-  //9
-  Scanner sc9 = new Scanner(System.in);
+  // ZADANIE WYKONANE PRZEZ JAKUBA BUJAKIEWICZA //
 
+  //9
   System.out.print("Podaj swoja wage w kg: ");
-  double waga = sc9.nextDouble();
+  double waga = sc.nextDouble();
 
   System.out.print("Podaj swoj wzrost w metrach: ");
-  double wzrost = sc9.nextDouble();
+  double wzrost = sc.nextDouble();
 
   double bmi = waga / (wzrost * wzrost);
 
   System.out.println("Twoje BMI wynosi: " + bmi);
 
-  if(bmi < 18.5){
+  if (bmi < 18.5) {
     System.out.println("Niedowaga");
-  }else if(bmi <= 24.9){
+  } else if (bmi <= 24.9) {
     System.out.println("Waga prawidlowa");
-  }else{
+  } else {
     System.out.println("Nadwaga");
   }
 
-  //10
-  Scanner sc10 = new Scanner(System.in);
+  // ZADANIE WYKONANE PRZEZ JAKUBA BUJAKIEWICZA //
 
+  //10
   double cena;
   int raty;
 
-  while(true){
+  while (true) {
     System.out.print("Podaj cene towaru (100 - 10000 zl): ");
-    cena = sc10.nextDouble();
+    cena = sc.nextDouble();
 
-    if(cena >= 100 && cena <= 10000){
+    if (cena >= 100 && cena <= 10000) {
       break;
     }
 
     System.out.println("Bledna cena! Podaj ponownie.");
   }
 
-  while(true){
+  while (true) {
     System.out.print("Podaj liczbe rat (6 - 48): ");
-    raty = sc10.nextInt();
+    raty = sc.nextInt();
 
-    if(raty >= 6 && raty <= 48){
+    if (raty >= 6 && raty <= 48) {
       break;
     }
 
@@ -176,11 +186,11 @@ System.out.println("Czy jestes pelnoletni? " + pelnoletni);
 
   double oprocentowanie;
 
-  if(raty >= 6 && raty <= 12){
+  if (raty >= 6 && raty <= 12) {
     oprocentowanie = 0.025;
-  }else if(raty >= 13 && raty <= 24){
+  } else if (raty >= 13 && raty <= 24) {
     oprocentowanie = 0.05;
-  }else{
+  } else {
     oprocentowanie = 0.10;
   }
 
@@ -189,21 +199,21 @@ System.out.println("Czy jestes pelnoletni? " + pelnoletni);
 
   System.out.println("Miesieczna rata wynosi: " + rata + " zl");
 
-  //11
-  Scanner sc11 = new Scanner(System.in);
+  // ZADANIE WYKONANE PRZEZ JAKUBA BUJAKIEWICZA //
 
+  //11
   System.out.println("Prosty kalkulator");
 
   System.out.print("Podaj pierwsza liczbe: ");
-  double liczba1 = sc11.nextDouble();
+  double liczba1 = sc.nextDouble();
 
   System.out.print("Podaj symbol operacji (+, -, *, /): ");
-  char operacja = sc11.next().charAt(0);
+  char operacja = sc.next().charAt(0);
 
   System.out.print("Podaj druga liczbe: ");
-  double liczba2 = sc11.nextDouble();
+  double liczba2 = sc.nextDouble();
 
-  switch(operacja){
+  switch (operacja) {
     case '+':
       System.out.println("Wynik: " + (liczba1 + liczba2));
       break;
@@ -217,9 +227,9 @@ System.out.println("Czy jestes pelnoletni? " + pelnoletni);
       break;
 
     case '/':
-      if(liczba2 == 0){
+      if (liczba2 == 0) {
         System.out.println("Nie mozna dzielic przez zero");
-      }else{
+      } else {
         System.out.println("Wynik: " + (liczba1 / liczba2));
       }
       break;
@@ -228,4 +238,5 @@ System.out.println("Czy jestes pelnoletni? " + pelnoletni);
       System.out.println("Bledny symbol operacji");
   }
 
+  // ZADANIE WYKONANE PRZEZ JAKUBA BUJAKIEWICZA //
 }
